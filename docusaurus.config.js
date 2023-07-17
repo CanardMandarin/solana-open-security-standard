c5082b25f37346381d746cb38f6a11c6cef3951b
CanardMandarin <thibault@opcodes.fr>
2023-07-17 09:00:50 +0000

    fix: config baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://solana-open-security-standard.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/solana-open-security-standard/",
   trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
